a4f46e5c52de4efb92c141ca9210919bf06640c3
Jalen Lane <jalen.lane7684@outlook.com>
2024-12-10 23:39:28 -0500

    Adding all pictures with alt text

--- project4/js/main_script.js
+++ project4/js/main_script.js
@@ -19,3 +19,26 @@ window.addEventListener('scroll', () => {
         }
     }
 })
+
+
+const updateDateAndTime = () => {
+        const now = new Date();
+        const formattedDate = now.toLocaleDateString();
+        const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        const dateTimeString = `Last Updated: ${formattedDate} ${formattedTime}`;
+    
+        const footer = document.querySelector('footer');
+        const dateTimeElement = footer.querySelector('.date-time');
+    
+        if (!dateTimeElement) {
+          const dateTimeElement = document.createElement('p');
+          dateTimeElement.classList.add('date-time');
+          footer.appendChild(dateTimeElement);
+        }
+    
+        dateTimeElement.textContent = dateTimeString;
+    
+};
+
+updateDateAndTime();
+setInterval(updateDateTime, 60000);
