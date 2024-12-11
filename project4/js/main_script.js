cf5819fc8c0a720c8ae70398ee8cf8c8abc6c467
Jalen Lane <jalen.lane7684@outlook.com>
2024-12-11 00:50:14 -0500

    Finalizing Project

--- project4/js/main_script.js
+++ project4/js/main_script.js
@@ -12,33 +12,10 @@ window.addEventListener('scroll', () => {
 
     if (currentURL.includes("/index")) {
         const navbar = document.getElementById('home-navbar');
-        if (scrollPosition >= 415) {
+        if (scrollPosition >= 15) {
             navbar.style.backgroundColor = "#539191";
         } else {
             navbar.style.backgroundColor = "rgba(27, 27, 27, 0)";
         }
     }
 })
-
-
-const updateDateAndTime = () => {
-        const now = new Date();
-        const formattedDate = now.toLocaleDateString();
-        const formattedTime = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-        const dateTimeString = `Last Updated: ${formattedDate} ${formattedTime}`;
-    
-        const footer = document.querySelector('footer');
-        const dateTimeElement = footer.querySelector('.date-time');
-    
-        if (!dateTimeElement) {
-          const dateTimeElement = document.createElement('p');
-          dateTimeElement.classList.add('date-time');
-          footer.appendChild(dateTimeElement);
-        }
-    
-        dateTimeElement.textContent = dateTimeString;
-    
-};
-
-updateDateAndTime();
-setInterval(updateDateTime, 60000);
